973f5085f0d8249411c128683ac2bfc946ad124b
KingOfAllK1ngs <48189158+KingOfAllK1ngs@users.noreply.github.com>
2020-02-19 18:58:05 +0200

    sdhdhsdhfh

---
+++ js/smoothScrolling.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#list-example").on("click","a", function (event) {
+
+        event.preventDefault();
+ 
+        var id  = $(this).attr('href'),
+ 
+            top = $(id).offset().top;
+         
+        $('body,html').animate({scrollTop: top}, 500);
+    });
+});
