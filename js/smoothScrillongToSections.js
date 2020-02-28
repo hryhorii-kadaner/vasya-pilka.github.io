987004aa62c2b242969034051a1381d8f6501893
MrGregor228 <kadaner.grisha@gmail.com>
2020-02-28 17:11:43 +0200

    Update smoothScrillongToSections.js

--- js/smoothScrillongToSections.js
+++ js/smoothScrillongToSections.js
@@ -1,51 +1,58 @@
-$(document).ready(function(){
-
+$(document).ready(function(){    
+    var htmlWindowWidth = document.documentElement.clientWidth;
+    
     setInterval(function () {
-        var i = document.documentElement.clientWidth;
-        console.log(i);
+        var htmlWindowWidth = document.documentElement.clientWidth;
+        console.log("Ширина экрана: " + htmlWindowWidth);
     }, 500);
 
-    if (document.documentElement.clientWidth > 768) {
-        console.log("Ширина монитора > 992px. Window.clientWidth = " + document.documentElement.clientWidth);
+    if (htmlWindowWidth > 768) {
+        console.log("Ширина монитора > 992px. Window.clientWidth = " + htmlWindowWidth);
         
         $("#toPortfolioFirst").on("click", function (event) {
             event.preventDefault();
             var id  = $(this).attr('href'),
                 top = $(id).offset().top / 2;
             $('body,html').animate({scrollTop: top}, 1000);
+            return htmlWindowWidth;
         });
         $("#toGallereyFirst").on("click", function (event) {
             event.preventDefault();
             var id  = $(this).attr('href'),
                 top = $(id).offset().top;
             $('body,html').animate({scrollTop: top}, 1000);
+            return htmlWindowWidth;
         });
         $("#toContactsFirst").on("click", function (event) {
             event.preventDefault();
             var id  = $(this).attr('href'),
                 top = $(id).offset().top;
             $('body,html').animate({scrollTop: top}, 1000);
+            return htmlWindowWidth;
         });
     } else {
-        console.log("Ширина монитора < 992px. Window.clientWidth =  " + document.documentElement.clientWidth);
+        console.log("Ширина монитора < 992px. Window.clientWidth =  " + htmlWindowWidth);
         
         $("#toPortfolioSecond").on("click", function (event) {
             event.preventDefault();
             var id  = $(this).attr('href'),
                 top = $(id).offset().top - 250;
             $('body,html').animate({scrollTop: top}, 1000);
+            return htmlWindowWidth;
         });
         $("#toGallereySecond").on("click", function (event) {
             event.preventDefault();
             var id  = $(this).attr('href'),
                 top = $(id).offset().top - 100;
             $('body,html').animate({scrollTop: top}, 1000);
+            return htmlWindowWidth;
         });
         $("#toContactsSecond").on("click", function (event) {
             event.preventDefault();
             var id  = $(this).attr('href'),
                 top = $(id).offset().top;
             $('body,html').animate({scrollTop: top}, 1000);
+            return htmlWindowWidth;
         });
     }
     
