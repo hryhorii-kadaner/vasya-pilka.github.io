c7610a16bd8ae5a6e7fb141563db39bdaa3b9b63
MrGregor228 <kadaner.grisha@gmail.com>
2020-03-05 13:10:43 +0200

    lszkihgbvljhseyesjh

--- galereya/js/links.js
+++ galereya/js/links.js
@@ -1,30 +1,35 @@
-var firstImgCaption = document.getElementById('firstImgCaption'),
-    secondImgCaption = document.getElementById('secondImgCaption'),
-    thirdImgCaption = document.getElementById('thirdImgCaption'),
-    fourthImgCaption = document.getElementById('fourthImgCaption'),
-    fifthImgCaption = document.getElementById('fifthImgCaption'),
-    sixImgCaption = document.getElementById('sixImgCaption');
+// var firstImgCaption = document.getElementById('firstImgCaption'),
+//     secondImgCaption = document.getElementById('secondImgCaption'),
+//     thirdImgCaption = document.getElementById('thirdImgCaption'),
+//     fourthImgCaption = document.getElementById('fourthImgCaption'),
+//     fifthImgCaption = document.getElementById('fifthImgCaption'),
+//     sixImgCaption = document.getElementById('sixImgCaption');
 
-function firstFun() {
-    document.location.href = "https://www.facebook.com/VasylPilka/";
-}
+// function firstFun() {
+//     document.location.href = "https://www.facebook.com/VasylPilka/";
+// }
 
-function secondFun() {
-    document.location.href = "https://www.facebook.com/VasylPilka/";
-}
+// function secondFun() {
+//     document.location.href = "https://www.facebook.com/VasylPilka/";
+// }
 
-function thirdFun() {
-    document.location.href = "https://www.facebook.com/VasylPilka/";
-}
+// function thirdFun() {
+//     document.location.href = "https://www.facebook.com/VasylPilka/";
+// }
 
-function fourthFun() {
-    document.location.href = "https://www.facebook.com/VasylPilka/";
-}
+// function fourthFun() {
+//     document.location.href = "https://www.facebook.com/VasylPilka/";
+// }
 
-function fifthFun() {
-    document.location.href = "https://www.facebook.com/VasylPilka/";
-}
+// function fifthFun() {
+//     document.location.href = "https://www.facebook.com/VasylPilka/";
+// }
 
-function sixFun() {
-    document.location.href = "https://www.facebook.com/VasylPilka/";
-}
+// function sixFun() {
+//     document.location.href = "https://www.facebook.com/VasylPilka/";
+// }
+
+$('[data-fancybox="gallery"]').fancybox({
+    animationEffect: "zoom",
+    transitionEffect: "circular"
+});
